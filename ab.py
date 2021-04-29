438c994b71fd220c8a7461c39c0db6627d4e1075
Abdul Basit Minhas <52023076+Tech-abm@users.noreply.github.com>
2021-04-29 00:29:55 -0700

    Create ab.py

---
+++ ab.py
@@ -0,0 +1,9 @@
+import os
+import datetime
+import random
+import uuid
+import re
+
+print("----> Blueforce-Abm Cmd has been deleted")
+print("----> New and update toos coming soon")
+print("----> sorry bro")
